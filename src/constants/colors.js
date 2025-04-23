9ec43418bc72687baf4de1487f656928bb11b8db
Mark Platt <mark@mrkplt.com>
2025-04-22 21:19:13 -0400

    clean up dead code

---
+++ src/constants/colors.js
@@ -0,0 +1,17 @@
+// &:active {
+//     background: #dcdcdc;
+//   }
+
+//   &.primary {
+//     background: #4CAF50;
+//     color: white;
+
+//     &:hover {
+//       background: #45a049;
+//     }
+
+//     &:active {
+//       background: #409145;
+//     }
+//   }
+// `;
